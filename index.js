c386dd830ce9a8c94e8ba548115c4e1b8dd6cb44
A. Dunn <alexdunnmobile@gmail.com>
2019-02-04 13:52:54 -0800

    Attempted to add a modal - modal does not appear on click

--- index.js
+++ index.js
@@ -7,12 +7,31 @@ function scrambleTeamNames() {
 
     for (i = 0; i < j - 1; i++) {
         var swapIndex = Math.floor(Math.random() * (j - i) + i);
-
         var temp = team[i].innerHTML;
 
-        // alert("swap->i");
         team[i].innerHTML = team[swapIndex].innerHTML;
-        // alert("temp->swap");
         team[swapIndex].innerHTML = temp;
     }
 }
+
+//Modal display functionality
+
+var myModal = document.getElementById("myModal");
+var modalLaunchButton = document.getElementById("modalLaunchButton");
+var span = document.getElementsByClassName("close")[0];
+
+modalLaunchButton.onclick = function () {
+    alert("triggered");
+    myModal.style.display = "block";
+}
+
+span.onclick = function () {
+    myModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == myModal) {
+//         modal.style.display = "none";
+//     }
+// }
